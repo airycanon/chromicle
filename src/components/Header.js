8a6f7aed720a6d3f0bdcaab54fd35a19bac79f44
airycanon <airycanon@airycanon.me>
2017-08-17 10:56:34 +0800

    Fix header affix.

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,36 +8,34 @@ export default class Header extends Component {
     render() {
         const {Header} = Layout;
         const {viewStore} = this.props;
-        if (!viewStore.hasSelected) {
-            return (<Affix>
-                <Header>
-                    <Row>
-                        <Col span={6} offset={6}>
-                            <DatePicker.RangePicker onChange={this.onChange.bind(this)}/>
-                        </Col>
-                        <Col className="header-right" span={6}>
-                            <div className="logo"/>
-                            <Input.Search placeholder="搜索历史记录" onSearch={this.onSearch.bind(this)}/>
-                        </Col>
-                    </Row>
-                </Header></Affix>)
+
+        let headerLeft, headerRight;
+        if (viewStore.hasSelected) {
+            headerLeft = <Col span={6} offset={6}>
+                <span className="header-selected-info">已选择 {viewStore.selectedHistories.length} 项</span>
+            </Col>;
+            headerRight = <Col span={6} className="header-right">
+                <Col span={6}>
+                    <Button ghost onClick={this.onCancel.bind(this)}>取消</Button>
+                </Col>
+                <Col>
+                    <Button type="danger" onClick={this.onRemove.bind(this)} ghost>删除</Button>
+                </Col>
+            </Col>;
         } else {
-            return (<Affix><Header>
-                <Row>
-                    <Col span={6} offset={6}>
-                        <span style={{color: '#aaa', fontSize: 14}}>已选择 {viewStore.selectedHistories.length} 项</span>
-                    </Col>
-                    <Col span={6} className="header-right">
-                        <Col span={6}>
-                            <Button ghost onClick={this.onCancel.bind(this)}>取消</Button>
-                        </Col>
-                        <Col>
-                            <Button type="danger" onClick={this.onRemove.bind(this)} ghost>删除</Button>
-                        </Col>
-                    </Col>
-                </Row>
-            </Header></Affix>)
+            headerLeft = <Col span={6} offset={6}>
+                <DatePicker.RangePicker onChange={this.onChange.bind(this)}/>
+            </Col>;
+            headerRight = <Col className="header-right" span={6}>
+                <Input.Search placeholder="搜索历史记录" onSearch={this.onSearch.bind(this)}/>
+            </Col>
         }
+
+        return (<Header>
+            <Affix>
+                <Row className="header-row" style={{}}>{headerLeft}{headerRight}</Row>
+            </Affix>
+        </Header>);
     }
 
     onChange(date, dateString) {
